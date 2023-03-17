19add84475061018d30cbd530fb19fa789e0cc66
KindyyDev <kindnessalawode2017@gmail.com>
2023-03-17 11:07:40 -0700

    A program that checks if a number is greater than 5 and less than 6

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - program that uses the last digit to check some conditions
+ *
+ * Checks:
+ * if the last digit is greater than 5
+ * if the last digit is 0
+ * if the last digit is less than 6 and no 0
+ *
+ * Declare a variable last_digit to hold the last digit of n
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	rand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
+	if (last_digit < 6 && last_digit != 0)
+	{
+		printf("Last digit of %i is %i less than 6 and not 0\n", n, last_digit);
+	}
+	else if (last_digit == 0)
+	{
+		printf("Last digit of %i is %i and is 0\n", n, last_digit);
+	}
+	else
+	{
+		printf("Last digit of %i is %i and is greater than 5", n, last_digit);
+	}
+
+	return (0);
+}
