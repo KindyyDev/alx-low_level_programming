59fa2e5c08d02e7f0950e263f0dabd56ad5816d3
KindyyDev <kindnessalawode2017@gmail.com>
2023-03-21 15:40:06 -0700

    A function that checks for lowercase character

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,5 +9,13 @@ void print_alphabet(void);
 
 void print_alphabet_x10(void);
 
-void putformat(int n);
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
 #endif
